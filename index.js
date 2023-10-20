f161c640f2e35ff32f03c83d4b04cc6e0f4c0bd1
RonnieTz <ronis.tzol@gmail.com>
2023-10-20 21:44:23 +0100

    create API

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(express.static("react-app/dist"));
+app.use(cors({ origin: "*" }));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "api works!" });
+});
 
 app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/react-app/dist/index.html");
