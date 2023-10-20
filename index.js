f69462ceaf08a54c565d399805f962e5c957911b
RonnieTz <ronis.tzol@gmail.com>
2023-10-20 23:18:03 +0100

    add get post delete and patch requests

--- index.js
+++ index.js
@@ -12,12 +12,39 @@ mongoose
   });
 
 const ItemSchema = mongoose.Schema({ name: String, checked: Boolean });
+const Item = mongoose.model("Item", ItemSchema);
 
 app.use(express.static("react-app/dist"));
 app.use(cors({ origin: "*" }));
+app.use(express.json());
 
-app.get("/api", (req, res) => {
-  res.json({ message: "api works!" });
+app.get("/api/get-items", async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+app.post("/api/save-item", async (req, res) => {
+  const { name } = req.body;
+  const item = new Item({ name, checked: false });
+  await item.save();
+  const response = await Item.find();
+  res.json(response);
+});
+
+app.delete("/api/delete-item", async (req, res) => {
+  const { id } = req.query;
+  await Item.findOneAndDelete({ _id: id });
+  const items = await Item.find();
+  res.json(items);
+});
+app.patch("/api/check-item", async (req, res) => {
+  const { id, checked } = req.query;
+  await Item.findOneAndUpdate(
+    { _id: id },
+    { checked: checked === "true" ? true : false }
+  );
+  const items = await Item.find();
+  res.json(items);
 });
 
 app.get("/*", (req, res) => {
